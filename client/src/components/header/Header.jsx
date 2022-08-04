f7d9972e44ef2bd36061ce7b9a53d92e799c6d1e
kalkidan12 <kalget12@gmail.com>
2022-08-04 23:37:05 +0300

    adding css

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Header.css";
+import { FaBars } from "react-icons/fa";
+import { FaAngleDown } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
 
 function Header() {
-	return <div>Header</div>;
+	return (
+		<header>
+			<nav className="nav-bar">
+				<div className="nav-logo">LOGO</div>
+				<div className="nav-toggler">
+					<span>
+						<FaBars />
+					</span>
+				</div>
+				<div className="nav-links">
+					<li className="nav-item">Home</li>
+					<li className="nav-item">Foods</li>
+					<li className="nav-item">Cart</li>
+					<li className="nav-item">Contact</li>
+				</div>
+				<div className="nav-right">
+					<li className="right-item">
+						<i className="cart-icon">
+							<FaShoppingCart />
+						</i>
+						<span className="cart-quantity">2</span>
+					</li>
+					<div className="dropdown">
+						<li className="dropbtn">
+							<i>
+								<FaUserCircle />
+							</i>
+
+							<i className="drop-icon">
+								<FaAngleDown />
+							</i>
+						</li>
+						<div className="dropdown-content">
+							<a href="#">Login</a>
+							<a href="#">Sign Up</a>
+						</div>
+					</div>
+					{/* <li className="right-item dropdown">account Icon</li> */}
+				</div>
+			</nav>
+		</header>
+	);
 }
 
 export default Header;
--- client/src/routes/AllRoutes.jsx
+++ client/src/routes/AllRoutes.jsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import React from "react";
+import "./route.css";
 import Home from "../pages/Home";
+import PageNotFound from "../pages/PageNotFound";
 
 function AllRoutes() {
 	return (
-		<>
+		<div className="main-body">
 			<Routes>
 				<Route path="/" exact element={<Home />} />
+				<Route path="*" element={<PageNotFound />} />
 			</Routes>
-		</>
+		</div>
 	);
 }
 
