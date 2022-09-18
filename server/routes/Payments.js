b59c575b06ac417714f1f0a64f089615048844cf
kalkidan12 <kalget12@gmail.com>
2022-09-19 00:20:56 +0300

    remove server folder

--- server/routes/Payments.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const {
-	VerifyPayment,
-	InitializePayent,
-} = require("../controllers/PaymentController");
-
-router.post("/initialize-payment", InitializePayent);
-router.get("verify/:tx_ref", VerifyPayment);
-module.exports = router;
